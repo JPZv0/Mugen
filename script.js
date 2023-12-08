2eedbe759aab6e0e29476d93c62dae18d3b8000b
JPZv0 <Jaedon.proctor1@gmail.com>
2023-12-07 22:08:03 -0600

    added basic dynamic boxes

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const container = document.getElementById('box-container');
+
+    container.addEventListener('mousedown', function(e) {
+        const startX = e.clientX;
+        const startY = e.clientY;
+
+        const box = document.createElement('textarea');
+        box.classList.add('dynamic-box');
+        box.style.left = `${startX}px`;
+        box.style.top = `${startY}px`;
+
+        container.appendChild(box);
+
+        function onMouseMove(e) {
+            const currentX = e.clientX;
+            const currentY = e.clientY;
+            const width = currentX - startX;
+            const height = currentY - startY;
+            box.style.width = `${width}px`;
+            box.style.height = `${height}px`;
+        }
+
+        container.addEventListener('mousemove', onMouseMove);
+
+        container.addEventListener('mouseup', function() {
+            container.removeEventListener('mousemove', onMouseMove);
+        }, { once: true });
+    });
+});
